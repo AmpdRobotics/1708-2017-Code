cdeec6a13abba039d96c2cd6b86b216932b1ed39
sstorck <soccerbot@gmail.com>
2017-02-15 20:12:35 -0500

    shifters
    shifters currently not working, need to be tested/worked on more

--- src/org/usfirst/frc/team1708/robot/RobotMap.java
+++ src/org/usfirst/frc/team1708/robot/RobotMap.java
@@ -31,15 +31,15 @@ public class RobotMap {
 	public static SpeedController driveRearRightMotor = new VictorSP(YELLOW);
 
 	public static SpeedController robotIntake = new CANTalon(BLUE);
-	
-	public static DoubleSolenoid gearShifter = new DoubleSolenoid(6,7);
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+
+	public static DoubleSolenoid gearShifter = new DoubleSolenoid(6, 7);
+	// For example to map the left and right motors, you could define the
+	// following variables to use with your drivetrain subsystem.
+	// public static int leftMotor = 1;
+	// public static int rightMotor = 2;
+
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
 }
