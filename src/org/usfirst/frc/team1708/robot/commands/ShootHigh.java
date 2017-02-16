932158c53c606aeaa6f765ff103c04726f260b96
sstorck <soccerbot@gmail.com>
2017-02-15 20:28:27 -0500

    Shooter
    Shooter tested, direction reversed

--- src/org/usfirst/frc/team1708/robot/commands/ShootHigh.java
+++ src/org/usfirst/frc/team1708/robot/commands/ShootHigh.java
@@ -21,7 +21,7 @@ public class ShootHigh extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.shooter.setShooterSpeed(.625);
+		Robot.shooter.setShooterSpeed(-.625);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
