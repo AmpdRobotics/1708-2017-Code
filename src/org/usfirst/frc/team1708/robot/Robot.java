20a532d06f06711adebb2bcf8fd2deb42141f4d6
sstorck <soccerbot@gmail.com>
2017-02-22 19:36:59 -0500

    camera
    camera added to dashboard, needs to be tested with robot

--- src/org/usfirst/frc/team1708/robot/Robot.java
+++ src/org/usfirst/frc/team1708/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team1708.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -37,8 +38,8 @@ public class Robot extends IterativeRobot {
 	public void robotInit() {
 		oi = new OI();
 		chooser = new SendableChooser();
-		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
+		CameraServer.getInstance().startAutomaticCapture();
 	}
 
 	/**
