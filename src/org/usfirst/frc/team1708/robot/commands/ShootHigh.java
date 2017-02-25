d61235d64af4d243a26b1d057a5f102327cc42c0
sstorck <soccerbot@gmail.com>
2017-02-25 13:58:15 -0500

    speed changed

--- src/org/usfirst/frc/team1708/robot/commands/ShootHigh.java
+++ src/org/usfirst/frc/team1708/robot/commands/ShootHigh.java
@@ -21,7 +21,7 @@ public class ShootHigh extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.shooter.setShooterSpeed(-.6);
+		Robot.shooter.setShooterSpeed(-.55);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
