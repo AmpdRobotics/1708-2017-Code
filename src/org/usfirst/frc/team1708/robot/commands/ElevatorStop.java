940e854e7a4bc9358ac6be854bd78d027033d4f4
sstorck <soccerbot@gmail.com>
2017-02-25 09:35:18 -0500

    elevator pause command group

--- src/org/usfirst/frc/team1708/robot/commands/ElevatorStop.java
+++ src/org/usfirst/frc/team1708/robot/commands/ElevatorStop.java
@@ -6,31 +6,35 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class ElevatorStop extends Command {
+	long startTime;
 
-    public ElevatorStop() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	public ElevatorStop() {
+		requires(Robot.elevator);
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		startTime = System.currentTimeMillis();
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.elevator.elevatorOff();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return System.currentTimeMillis() >= startTime + 200;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
