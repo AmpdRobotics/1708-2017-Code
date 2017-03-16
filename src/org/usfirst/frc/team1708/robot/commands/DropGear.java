3c289be183e3ca62f4d3ce8bc99deb56fe42a858
sstorck <soccerbot@gmail.com>
2017-03-16 08:55:29 -0400

    changed auto, gear timed

--- src/org/usfirst/frc/team1708/robot/commands/DropGear.java
+++ src/org/usfirst/frc/team1708/robot/commands/DropGear.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class DropGear extends Command {
-
+			long startTime;
     public DropGear() {
     	requires(Robot.gearDropper);
         // Use requires() here to declare subsystem dependencies
@@ -17,6 +17,7 @@ public class DropGear extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	startTime = System.currentTimeMillis();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -25,7 +26,7 @@ public class DropGear extends Command {
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	return 1000 + startTime <= System.currentTimeMillis();
     }
 
     // Called once after isFinished returns true
